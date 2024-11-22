df9515eb371d1648211f06e8aa8f6b29fd717653
Rawan <102298504+rawanazizsaad@users.noreply.github.com>
2024-11-22 14:58:53 +0200

    Updates

---
+++ precache-manifest.b0cce461d4436527662f14564572f545.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac2ac6bf30f9889171db97011ffcfc0",
+    "url": "/Airports/index.html"
+  },
+  {
+    "revision": "5558063e8249b049e981",
+    "url": "/Airports/static/css/2.4e9f5f40.chunk.css"
+  },
+  {
+    "revision": "3693f3bf910e2b8c10eb",
+    "url": "/Airports/static/css/main.40064a40.chunk.css"
+  },
+  {
+    "revision": "5558063e8249b049e981",
+    "url": "/Airports/static/js/2.65ecea91.chunk.js"
+  },
+  {
+    "revision": "a46f58956e3b48cc08a8",
+    "url": "/Airports/static/js/3.1629fdc3.chunk.js"
+  },
+  {
+    "revision": "3693f3bf910e2b8c10eb",
+    "url": "/Airports/static/js/main.b3f6dca8.chunk.js"
+  },
+  {
+    "revision": "358bef5c3d7ecc7d06f3",
+    "url": "/Airports/static/js/runtime~main.c6e944a9.js"
+  },
+  {
+    "revision": "1931427c23716bb4ff98d03627f7137c",
+    "url": "/Airports/static/media/fa-brands-400.1931427c.ttf"
+  },
+  {
+    "revision": "b6356c957274676e6571c1ff5e11c9a8",
+    "url": "/Airports/static/media/fa-brands-400.b6356c95.woff2"
+  },
+  {
+    "revision": "023a4a925fa3fce0f66b769ef6bbb264",
+    "url": "/Airports/static/media/fa-regular-400.023a4a92.woff2"
+  },
+  {
+    "revision": "a0cc1c8265e3163aa654a5284ea11ace",
+    "url": "/Airports/static/media/fa-regular-400.a0cc1c82.ttf"
+  },
+  {
+    "revision": "25914cfeafced317e1a0372187fbb2b9",
+    "url": "/Airports/static/media/fa-solid-900.25914cfe.ttf"
+  },
+  {
+    "revision": "6c4eee562650e53cee32496bdfbe534b",
+    "url": "/Airports/static/media/fa-solid-900.6c4eee56.woff2"
+  },
+  {
+    "revision": "71808e147dc6d82c198a4ba292c0cb69",
+    "url": "/Airports/static/media/fa-v4compatibility.71808e14.ttf"
+  },
+  {
+    "revision": "c9e50ccabe9e36f370272197595ea1e5",
+    "url": "/Airports/static/media/fa-v4compatibility.c9e50cca.woff2"
+  },
+  {
+    "revision": "7c8d991e8a2be6abd4986c3e00c35aec",
+    "url": "/Airports/static/media/face.7c8d991e.png"
+  },
+  {
+    "revision": "450c5f918caa1be3a0c0c30ca541abac",
+    "url": "/Airports/static/media/fly.450c5f91.jpg"
+  },
+  {
+    "revision": "cc8d90e1e4335d7a0a46134e5c40feee",
+    "url": "/Airports/static/media/git.cc8d90e1.png"
+  },
+  {
+    "revision": "812cb02f6f2732937ddf0cc5e00d951e",
+    "url": "/Airports/static/media/img.812cb02f.png"
+  },
+  {
+    "revision": "4a87f83d39e51670d1b6384841df2c98",
+    "url": "/Airports/static/media/user.4a87f83d.svg"
+  }
+]);
